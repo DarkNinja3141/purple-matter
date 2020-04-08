7d39f042114c2370dc0a87456bec4f6d17daa7d1
DarkNinja3141 <DarkNinja3141@gmail.com>
2020-04-07 22:31:45 -0400

    Add Aionios Fuel Add temporary purple matter recipe

--- src/main/java/darkninja2462/purplematter/PurpleMatter.java
+++ src/main/java/darkninja2462/purplematter/PurpleMatter.java
@@ -17,7 +17,7 @@ public class PurpleMatter
 {
     public static final String MODID = "purplematter";
     public static final String NAME = "Purple Matter";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     private static Logger logger;
 
@@ -30,8 +30,7 @@ public class PurpleMatter
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 
 }
---
+++ src/main/java/darkninja2462/purplematter/common/item/ItemAioniosFuel.java
@@ -0,0 +1,8 @@
+package darkninja2462.purplematter.common.item;
+
+import darkninja2462.purplematter.mod.SimpleModItem;
+import net.minecraft.item.Item;
+
+@SimpleModItem("aionios_fuel")
+public class ItemAioniosFuel extends Item {
+}
