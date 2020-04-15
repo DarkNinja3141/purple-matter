b05d597cc7dac23548ccacdaf2547910d0a41c84
DarkNinja3141 <DarkNinja3141@gmail.com>
2020-04-15 09:08:23 -0400

    Add EMC capacity tooltip for all IItemEMC items

--- src/main/java/darkninja2462/purplematter/handlers/HandlerTooltips.java
+++ src/main/java/darkninja2462/purplematter/handlers/HandlerTooltips.java
@@ -3,6 +3,7 @@ package darkninja2462.purplematter.handlers;
 import darkninja2462.purplematter.PurpleMatter;
 import darkninja2462.purplematter.common.item.ItemHochStar;
 import darkninja2462.purplematter.util.ItemNBTUtils;
+import moze_intel.projecte.api.item.IItemEmc;
 import moze_intel.projecte.utils.Constants;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
@@ -12,7 +13,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static darkninja2462.purplematter.handlers.HandlerItems.*;
+import static moze_intel.projecte.gameObjs.ObjHandler.kleinStars;
 
 @Mod.EventBusSubscriber(modid = PurpleMatter.MODID, value = Side.CLIENT)
 public class HandlerTooltips {
@@ -23,9 +28,12 @@ public class HandlerTooltips {
     public static void onTooltip(ItemTooltipEvent event) {
         ItemStack stack = event.getItemStack();
         if (stack.isEmpty()) return;
-        if(stack.getItem() == hoch_star) {
-            long capacity = ItemHochStar.getFullCapacity(stack);
-            event.getToolTip().add(TextFormatting.YELLOW + I18n.format(TRANSLATEKEY_STOREDEMC_TOOLTIP) + ": " + TextFormatting.RESET + Constants.EMC_FORMATTER.format(capacity));
+        if(stack.getItem() instanceof IItemEmc) {
+            long capacity = ((IItemEmc) stack.getItem()).getMaximumEmc(stack);
+            List<String> tooltip = event.getToolTip();
+            //Insert before "Stored EMC:" or at end
+            int index = IntStream.range(0, tooltip.size()).filter((i) -> tooltip.get(i).contains(I18n.format("pe.emc.storedemc_tooltip"))).findFirst().orElse(tooltip.size());
+            tooltip.add(index, TextFormatting.YELLOW + I18n.format(TRANSLATEKEY_STOREDEMC_TOOLTIP) + ": " + TextFormatting.RESET + Constants.EMC_FORMATTER.format(capacity));
         }
     }
 
